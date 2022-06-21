cf8a9bb19c24a2cedfef687c702a1a5a0de1f0b4
Your Name <you@example.com>
2022-06-21 14:16:25 +0300

    sqf

---
+++ js/index.js
@@ -0,0 +1,9 @@
+new fullpage('#fullpage', {
+   autoScrolling: true,
+   scrollHorizontally: true,
+   navigation: true,
+   anchors: ['section1', 'section2', 'section3']
+});
+
+//methods
+fullpage_api.setAllowScrolling(false);
